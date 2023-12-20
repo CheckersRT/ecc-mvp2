3e3670918cb68ddbdc67b2ff4c609b71d555a47b
Tom Dent <tomdent991@hotmail.com>
2023-12-20 13:50:18 +0100

    openai working, now adding google vision

---
+++ pages/api/extractText.js
@@ -0,0 +1,5 @@
+import { v1 } from "@google-cloud/vision";
+
+export default async function extractText(req, res) {
+    
+}
---
+++ pages/api/getInfo.js
@@ -0,0 +1,31 @@
+import OpenAI from "openai"
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+const openai = new OpenAI({
+	apiKey: 
+  // process.env.openaiapikey,
+  "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"
+
+});
+
+
+export default async function getInfo(req, res) {
+  console.log("Request body: ", req.body)
+
+  const { inputText } = req.body
+
+  const prompt = `
+  Extract the composer, song title and music publicher from this text: ${inputText}. 
+  `
+
+  const response = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: prompt }],
+    model: 'gpt-3.5-turbo',
+  });  
+
+  const completion = response.choices[0].message.content
+
+  res.status(200).json({ output: completion});
+  // res.status(200).json({ name: 'John Doe' })
+}
